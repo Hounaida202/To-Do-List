8f0e8d376169adcf081e616a1262e030e3d158c0
Hounaida202 <etrichih@gmail.com>
2024-11-02 22:48:28 +0100

    ajouter les conditions pour que le status se fonctionne

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const boutonadd = document.getElementById("monbouton");
-const sectionadd = document.getElementById("modal");
-const exitbouton = document.getElementById("exit");
+let boutonadd = document.getElementById("monbouton");
+let sectionadd = document.getElementById("modal");
+let exitbouton = document.getElementById("exit");
+let title = document.getElementById('titleinput');
+let description = document.getElementById('descriptioninput');
+let date = document.getElementById('dateinput');
+let submit = document.getElementById('submit');
+let status =document.getElementById('status');
+
+
+
+
+
 
 boutonadd.onclick = function (){
 sectionadd.style.display = 'block';
@@ -11,42 +21,216 @@ exitbouton.onclick = function (){
   sectionadd.style.display="none" ;
 }
 
+status.addEventListener('change', function () {
 
-// const modal = document.getElementById("modal");
-// const form = modal.querySelector("form");
-// let submitBtn = document.getElementById("submit");
-
-// console.log(form)
-
-// submitBtn.addEventListener("click", function() {
-//     let title = document.getElementById("title").value;
-//     let description = document.getElementById("description").value;
-
-//     console.log(title, description)
-// });
+  if (status.value === 'todo') {
+      submit.onclick = todo;
+  } else if (status.value === 'doing') {
+      submit.onclick = inprogress;
+  } else {
+      submit.onclick = done;
+  }
+  
+});
 
 
 
-submit.onclick = function (){
-
-    const tableau=[];
-    const title1 = document.getElementById("titleinput").value;
-const description1 = document.getElementById("descriptioninput").value;
-const date1 = document.getElementById("dateinput").value;
-
-// ce tableau stock les inputs entrées
-
-tableau.push(title1,description1,date1);
 
 
-// pour afficher ce qui est stocké
+///--------------------------------------------fonction to do ------------------------------------------
+////// fonction pour stocker
 
-tableau.forEach((valeur) => {
-    document.getElementById("containeradd").innerHTML += `<p>${valeur}</p>`;
-  });
+let tableaustocker1=[];
 
+function todo(){
 
+let object={
+title:title.value ,
+description:description.value ,
+date:date.value ,
+}
+tableaustocker1.push(object)
+
+clear1();
+afficher1();
+sortir1();
+}
+
+////////// fonction clear
+
+function clear1 () {
+title.value="";
+description.value="";
+date.value="";
 
 }
+
+
+///////// fonction pour afficher les donnés 
+
+function afficher1(){  
+let table=""; /// a appeler dans innerhtml
+for(let i=0 ; i<tableaustocker1.length; i++){
+table+=`
+<div class=" tasks-container  mt-2 border mt-3 w-75" id="to-do-container" >
+                 <div class=" border-start border-danger border-5 "> 
+                    <div class="task px-4 py-2 w-100">
+                        <div id="containeradd">
+                            <h3>${tableaustocker1[i].title}</h3>
+                            <p>${tableaustocker1[i].description}</p>
+                            <p>${tableaustocker1[i].date}</p>
+                            <button type="button" class="btn btn-danger btn-sm">Delet</button>
+                            <button type="button" class="btn btn-warning btn-sm">Edit</button>
+                        </div>
+                  </div> 
+                </div>
+             </div>  
+`;
+document.getElementById('container1').innerHTML=table;
+}
+}
+
+// fonction pour sortir apres ajouter 
+
+function sortir1(){
+
+  sectionadd.style.display="none" ;
+
+}
+
+
+
+///--------------------------------------------fonction in progress ------------------------------------------
+
+
+  let tableaustocker2=[];
+
+  function inprogress(){
+  
+  let object={
+  title:title.value ,
+  description:description.value ,
+  date:date.value ,
+  }
+  tableaustocker2.push(object)
+  
+  clear2();
+  afficher2();
+  sortir2();
+  }
+  
+  ////////// fonction clear
+  
+  function clear2 () {
+  title.value="";
+  description.value="";
+  date.value="";
+  
+  }
+  
+  
+  ///////// fonction pour afficher les donnés 
+  
+  function afficher2(){  
+  let table=""; /// a appeler dans innerhtml
+  for(let i=0 ; i<tableaustocker2.length; i++){
+  table+=`
+  <div class=" tasks-container  mt-2 border mt-3 w-75" id="to-do-container" >
+                   <div class=" border-start border-danger border-5 "> 
+                      <div class="task px-4 py-2 w-100">
+                          <div id="containeradd">
+                              <h3>${tableaustocker2[i].title}</h3>
+                              <p>${tableaustocker2[i].description}</p>
+                              <p>${tableaustocker2[i].date}</p>
+                              <button type="button" class="btn btn-danger btn-sm">Delet</button>
+                              <button type="button" class="btn btn-warning btn-sm">Edit</button>
+                          </div>
+                    </div> 
+                  </div>
+               </div>  
+  `;
+  document.getElementById('container2').innerHTML=table;
+  }
+  }
+  
+  // fonction pour sortir apres ajouter 
+  
+  function sortir2(){
+  
+    sectionadd.style.display="none" ;
+  
+  }
+  
+  
+
+///--------------------------------------------fonction done ------------------------------------------
+
+
+    let tableaustocker3=[];
+
+
+
+
+    function done(){
+    
+    let object={
+    title:title.value ,
+    description:description.value ,
+    date:date.value ,
+    }
+    tableaustocker3.push(object)
+    
+    clear3();
+    afficher3();
+    sortir3();
+    }
+    
+    ////////// fonction clear
+    
+    function clear3 () {
+    title.value="";
+    description.value="";
+    date.value="";
+    
+    }
+    
+    
+    ///////// fonction pour afficher les donnés 
+    
+    function afficher3(){  
+    let table=""; /// a appeler dans innerhtml
+    for(let i=0 ; i<tableaustocker3.length; i++){
+    table+=`
+    <div class=" tasks-container  mt-2 border mt-3 w-75" id="to-do-container" >
+                     <div class=" border-start border-danger border-5 "> 
+                        <div class="task px-4 py-2 w-100">
+                            <div id="containeradd">
+                                <h3>${tableaustocker3[i].title}</h3>
+                                <p>${tableaustocker3[i].description}</p>
+                                <p>${tableaustocker3[i].date}</p>
+                                <button type="button" class="btn btn-danger btn-sm">Delet</button>
+                                <button type="button" class="btn btn-warning btn-sm">Edit</button>
+                            </div>
+                      </div> 
+                    </div>
+                 </div>  
+    `;
+    document.getElementById('container3').innerHTML=table;
+    }
+    }
+    
+    // fonction pour sortir apres ajouter 
+    
+    function sortir3(){
+    
+      sectionadd.style.display="none" ;
+    
+    }
+
+//------------------------------------------------------------------------------------------
+  
+
+
+
 
 
